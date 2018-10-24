116ba08dc89626ad09b881d8e3109cd0099d03b1
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-10-24 18:58:23 -0400

    working burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pro burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Alright burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Super burger", true);
